d3d51c4acca661676597b912e75c82f31bc6e9bf
PatriotRossii <patriotrossii2019@mail.ru>
2023-04-29 13:27:48 +0300

    Implement trivial difference based solution

--- lesson_00/02_buy_and_sell/main.cpp
+++ lesson_00/02_buy_and_sell/main.cpp
@@ -13,25 +13,25 @@ int main() {
 		std::cin >> x;
 	}
 
-	// Согласно условию, цена — целое число от 1 до 5000
-	int minimum_price = 5000;
-	int maximum_price = 1;
-	int buy_date{-1}, sell_date{-1};
+	int buy_date{-1};
+	int sell_date{-1};
+	int maximum_difference{-1};
 
-	for (auto i = static_cast<size_t>(0), size = prices.size(); i != size; ++i) {
-		auto price = prices[i];
-		if (price <= budget && price < minimum_price) {
-			minimum_price = price;
-			buy_date = i;
-			maximum_price = 1;
-		}
-		if (buy_date != -1 && price > maximum_price) {
-			maximum_price = price;
-			sell_date = i;
+	for (auto iIt = prices.begin(), begin = prices.begin(), end = prices.end(); iIt != end; ++iIt) {
+		int iElement = *iIt;
+		if (iElement > budget) continue;
+		for (auto jIt = iIt + 1; jIt != end; ++jIt) {
+			int jElement = *jIt;
+			int difference = jElement - iElement;
+			if (difference > maximum_difference) {
+				maximum_difference = difference;
+				buy_date = iIt - begin;
+				sell_date = jIt - begin;
+			}
 		}
 	}
 
-	if (buy_date < sell_date && minimum_price < maximum_price) {
+	if (buy_date < sell_date) {
 		std::cout << buy_date + 1 << ' ' << sell_date + 1 << '\n';
 	} else {
 		std::cout << 0 << ' ' << 0 << '\n';
